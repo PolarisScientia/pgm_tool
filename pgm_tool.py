e7167757e8562f7b67090c029ff76b88543ca437
Scott <2251386+PolarisScientia@users.noreply.github.com>
2020-03-02 10:05:38 -0500

    Update pgm_tool.py
    added raster dilation.
    changed crop_raster() to cropRaster().

--- pgm_tool.py
+++ pgm_tool.py
@@ -2,7 +2,7 @@
 
 import sys, getopt
 
-def crop_raster(raster, cropval=205):
+def cropRaster(raster, cropval=205):
     right = bottom = 0
     top = left = sys.maxsize
     row = col = 0
@@ -35,6 +35,30 @@ def crop_raster(raster, cropval=205):
         crop2.append(row[left:right])
     return crop2
 
+def dilateRaster(raster, size=5, occupied=0):
+    """Dilate the raster by convolution of size x size matrix of 1's
+        size parameter should be an odd value."""
+    i = int((size-1)/2)
+    row = 0
+    max_row = len(raster)-1
+    max_col = len(raster[1])-1
+    toDilate = []
+    for line in raster: #collect pixels to dilate first
+        col = 0
+        for value in line:
+            if value == occupied:
+                for rowmod in range(-i, i+1):
+                    for colmod in range(-i, i+1):
+                        if row+rowmod > 0 and col+colmod > 0 and row+rowmod <= max_row and col+colmod <= max_col:
+                            cVal = raster[row+rowmod][col+colmod]
+                            if cVal != occupied:
+                                toDilate.append((row+rowmod, col+colmod))
+            col = col+1
+        row = row+1
+    for tup in toDilate: #iterate through collected pixel tuples to set values
+        raster[tup[0]][tup[1]] = occupied
+    return raster
+
 def read_pgm(pgmf):
     """Return a raster of integers from a PGM as a list of lists."""
     assert pgmf.readline() == b'P5\n'
@@ -56,15 +80,16 @@ def main(argv):
     outputfile = ''
     crop = 0
     pgm = 0
+    dilate = 0
     try:
-        opts, args = getopt.getopt(argv,"hbci:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"hbcd:i:o:",["dilate=","ifile=","ofile="])
     except getopt.GetoptError:
-        print(str(err))
+        print(str(getopt.err))
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print("pgm_reading.py [-b] [-c] -i <inputfile> -o <outputfile>")
-            sys.ext()
+            print("pgm_tool.py [-b] [-c] [-d <size>] -i <inputfile> -o <outputfile>")
+            sys.exit(2)
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
@@ -75,13 +100,18 @@ def main(argv):
         elif opt == '-c':
             print("Cropping margins.")
             crop = 1
+        elif opt in ("-d", "--dilate"):
+            print("Dilating raster with size " + str(int(arg)) + ".")
+            dilate = int(arg)
     print("Input file is", inputfile)
     print("Output file is", outputfile)
     if inputfile:
         with open(inputfile, 'rb') as f:
             raster = read_pgm(f)
             if crop:
-                raster = crop_raster(raster, 205)
+                raster = cropRaster(raster, 205)
+            if dilate:
+                raster = dilateRaster(raster, dilate, 0)
             if outputfile and not pgm:
                 with open(outputfile, 'w+') as o:
                     for line in raster:
@@ -94,7 +124,7 @@ def main(argv):
                     #pythonic unpacking of list of lists above
                     height = len(raster)
                     width = len(raster[1])
-                    o.write(bytes('P5' + '\n' + '# CREATOR: pgm_reading.py 0.02\n', encoding='utf8'))
+                    o.write(bytes('P5' + '\n' + '# CREATOR: pgm_tool.py 0.02\n', encoding='utf8'))
                     o.write(bytes(str(width) + ' ' + str(height) + '\n' + str(255) + '\n', encoding='utf8'))
                     rasterbytes = bytes(flat_raster)
                     o.write(rasterbytes)
